0ee0c100ffc136598490165c154b8e1482ab9a5b
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-08-14 11:15:19 -0300

    se agrega docker a sistemas

---
+++ sistemas_operativos_III/2023/docker/dockerfiles/crearObjetos/Persona.php
@@ -0,0 +1,45 @@
+<?php class Persona {
+
+private $ci;
+private $nombre;
+private $apellido; 
+
+//Método constructor de la clase Persona
+function __construct(string $ciU, string $nombreU, string $apellidoU){
+    $this->ci = $ciU;
+    $this->nombre = $nombreU;
+    $this->apellido = $apellidoU;
+}
+
+public function getCi():String{
+    return $this->ci;
+}
+
+public function setCi(String $value):void{
+    $this->ci = $value;
+}
+
+public function getNombre():String{
+    return $this->nombre;
+}
+
+public function setNombre(String $value):void{
+    $this->nombre = $value;
+}
+
+public function getApellido():String{
+    return $this->apellido;
+}
+
+public function setApellido(String $value):void{
+    $this->apellido = $value;
+}
+
+//Método toString de la clase Persona
+public function toString():String{
+    return "CI: {$this->getCi()} Nombre: {$this->getNombre()} Apellido: {$this->getApellido()}";
+}
+
+}
+
+?>
