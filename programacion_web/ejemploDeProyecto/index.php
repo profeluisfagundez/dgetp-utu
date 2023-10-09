d13949626bb83db9c49507b87cab02e9ba5bc345
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-10-08 23:15:41 -0300

    se agrego el práctico 1

---
+++ programacion_web/ejemploDeProyecto/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de Estructura Semántica en HTML5</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Mi Página Web</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h2>Acerca de Nosotros</h2>
+            <p>Somos una empresa dedicada a...</p>
+        </section>
+
+        <section>
+            <h2>Nuestros Servicios</h2>
+            <ul>
+                <li>Servicio 1</li>
+                <li>Servicio 2</li>
+                <li>Servicio 3</li>
+            </ul>
+        </section>
+
+        <article>
+            <h2>Artículo Destacado</h2>
+            <p>Este es un artículo importante sobre...</p>
+        </article>
+    </main>
+
+    <aside>
+        <h3>Anuncios</h3>
+        <p>¡Oferta especial esta semana!</p>
+    </aside>
+
+    <footer>
+        <p>&copy; 2023 Mi Página Web. Todos los derechos reservados.</p>
+    </footer>
+
+    <script src="scripts.js"></script>
+</body>
+</html>
