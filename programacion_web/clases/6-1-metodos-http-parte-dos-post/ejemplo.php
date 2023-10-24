7c1fb5ecdb45e91e8d3ec260ecea2bbc6038a560
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-10-23 21:13:47 -0300

    modificaciones en clase

--- programacion_web/clases/6-1-metodos-http-parte-dos-post/ejemplo.php
+++ programacion_web/clases/6-1-metodos-http-parte-dos-post/ejemplo.php
@@ -7,6 +7,12 @@
     <?php
         $nombre = isset($_POST["nombre"]) ? $_POST["nombre"] : "";
         $email = isset($_POST["email"]) ? $_POST["email"] : "";
+        /*if (isset($_POST["nombre"])){
+            $nombre = $_POST["nombre"];
+        } else {
+            $nombre = " ";
+        }*/
+
     ?>
     <p>Muy buenos días, <?php echo $nombre; ?></p>
     <p>Su correo electrónico es: <?php echo $email; ?></p>
