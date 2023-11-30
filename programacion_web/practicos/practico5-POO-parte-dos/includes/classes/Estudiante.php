6502f4e5a2671030fb75f09b6eaa9bd9a145f2e5
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-11-30 09:07:13 -0300

    se modifica la logica de Estudiante

--- programacion_web/practicos/practico5-POO-parte-dos/includes/classes/Estudiante.php
+++ programacion_web/practicos/practico5-POO-parte-dos/includes/classes/Estudiante.php
@@ -8,6 +8,15 @@ class Estudiante {
     public $curso;
     public $materias = []; // Arreglo asociativo con nombre de materia y nota
 
+    /**
+     * Constructor de la clase Estudiante.
+     *
+     * @param string $ci Cédula de identidad.
+     * @param string $nombre Nombre del estudiante.
+     * @param string $apellido Apellido del estudiante.
+     * @param string $fechaNacimiento Fecha de nacimiento del estudiante.
+     * @param string $curso Curso del estudiante.
+     */
     public function __construct($ci, $nombre, $apellido, $fechaNacimiento, $curso) {
         $this->ci = $ci;
         $this->nombre = $nombre;
@@ -16,9 +25,21 @@ class Estudiante {
         $this->curso = $curso;
     }
 
+    /**
+     * Agrega una materia al estudiante.
+     *
+     * @param string $nombreMateria Nombre de la materia.
+     * @param float $nota Nota de la materia.
+     * @throws Exception Si la materia ya existe para este estudiante.
+     */
     public function agregarMateria($nombreMateria, $nota) {
-        $materia = new Materia($nombreMateria, $nota);
-        $this->materias[$nombreMateria] = $materia;
+        if (!isset($this->materias[$nombreMateria])) {
+            $materia = new Materia($nombreMateria, $nota);
+            $this->materias[$nombreMateria] = $materia;
+        } else {
+            // Lanzar una excepción si la materia ya existe
+            throw new Exception("La materia '$nombreMateria' ya existe para este estudiante.");
+        }
     }
 
 }
