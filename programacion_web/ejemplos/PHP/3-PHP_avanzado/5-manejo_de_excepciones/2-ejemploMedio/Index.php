2b284a0ccae488d84c8116f3eabbe5db845ec0cb
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-12-01 19:28:05 -0300

    se genera la excepción

--- programacion_web/ejemplos/PHP/3-PHP_avanzado/5-manejo_de_excepciones/2-ejemploMedio/Index.php
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/5-manejo_de_excepciones/2-ejemploMedio/Index.php
@@ -1,18 +1,16 @@
 <?php
 require_once('Excepciones.php');
+
 function dividir($a, $b) {
-    try {
-        if ($b === 0) {
-            throw new Excepciones("No se puede dividir entre cero");
-        }
-        $result = $a / $b;
-        return $result;
-    } catch (Excepciones $e) {
-        $e->handle();
+    if ($b === 0) {
+        throw new Excepciones("No se puede dividir entre cero", true);
     }
+    $result = $a / $b;
+    return $result;
 }
+
 try {
-    $resultado = dividir(10, 0);
+    $resultado = dividir(10, 2);
     echo $resultado . "\n";
 } catch (Excepciones $e) {
     echo $e->getMessage() . "\n";
