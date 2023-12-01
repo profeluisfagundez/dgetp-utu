800d6963de4bf5b65986827831f1465b7b62a1a7
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-12-01 19:52:38 -0300

    se genera el primer controlador

---
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/6-conexionBDMySQL/database/PDO/2/IngresosController.php
@@ -0,0 +1,24 @@
+<?php 
+
+class IngresosController {
+    /**
+    * Los 7 métodos que suelen tener los controladores:
+    * index: muestra la lista de todos los recursos.
+    * create: muestra un formulario para ingresar un nuevo recurso. (luego manda a llamar al método store).
+    * store: registra dentro de la base de datos el nuevo recurso.
+    * show: muestra un recurso específico.
+    * edit: muestra un formulario para editar un recurso. (luego manda a llamar al método update).
+    * update: actualiza el recurso dentro de la base de datos.
+    * destroy: elimina un recurso.
+     */
+    public function index(){}
+    public function create(){}
+    public function store(){}
+    public function show(){}
+    public function edit(){}
+    public function update(){}
+    public function destroy(){}
+
+}
+
+?>
