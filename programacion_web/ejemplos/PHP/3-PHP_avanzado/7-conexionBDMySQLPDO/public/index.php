86bfc30fe2c96c7e3af92594ff4428341ca79c58
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2023-12-15 09:27:04 -0300

    se agregan comentarios al router

--- programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/public/index.php
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/public/index.php
@@ -9,9 +9,15 @@ require_once("../app/enums/IngresoTipoEnum.php");
 require_once("../app/enums/RetiroTipoEnum.php");
 require_once("../routers/RouterHandler.php");
 
+// el operador de fusión ?? asigna lo que venga por defecto del método get, en caso de no tener nada
+//Asigna un valor vacio
 $slug = $_GET["slug"] ?? "";
+//Explode genera un array con el contenido de slug, usa como separador el simbolo de /
 $slug = explode("/", $slug);
+//Verificamos que la primer parte del array de slug es vacia, si lo es se le asigna /
+// En caso de no ser vacio, a la variable $resource se le asigna la primer parte del array
 $resource = $slug[0] == "" ? "/" : $slug[0];
+//A la variable id se le asigna el segundo valor del arreglo slug, en caso de ser vacio se le asigna null
 $id = $slug[1] ?? null;
 $router = new RouterHandler();
 
