e5abe57fa84a24078bf69764280fac6703fe41ac
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-01-19 20:08:57 -0300

    se agrega un fix al 404

--- programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/app/exceptions/404.php
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/app/exceptions/404.php
@@ -13,12 +13,23 @@
             background-color: #f4f4f4;
             color: #333;
             padding: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
         }
 
         header {
             background-color: #333;
             color: #fff;
             padding: 15px;
+            width: 100%;
+            position: fixed;
+            top: 0;
+            text-align: center;
+            z-index: 1000;
         }
 
         h1 {
@@ -28,12 +39,17 @@
 
         p {
             font-size: 1.2em;
-            margin-bottom: 30px;
+            margin-bottom: 10px;
         }
 
         a {
             color: #0066cc;
             text-decoration: none;
+            font-size: 1.2em;
+        }
+
+        footer {
+            margin-top: 10px;
         }
     </style>
 </head>
@@ -43,7 +59,9 @@
         <h1>Error 404 - Página no encontrada</h1>
     </header>
     <p>Lo sentimos, la página que estás buscando no se encuentra.</p>
-    <p><a href="/">Volver al inicio</a></p>
+    <footer>
+        <a href="/">Volver al inicio</a>
+    </footer>
 </body>
 
 </html>
