59a9a48d527de24460e471f723f8bed998914a47
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-02-01 22:24:51 -0300

    se actualiz0 404

--- programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/app/exceptions/404.php
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/7-conexionBDMySQLPDO/app/exceptions/404.php
@@ -5,53 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error 404 - Página no encontrada</title>
-    <link rel="stylesheet" href="/css/inicioStyle.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            background-color: #f4f4f4;
-            color: #333;
-            padding: 50px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            margin: 0;
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 15px;
-            width: 100%;
-            position: fixed;
-            top: 0;
-            text-align: center;
-            z-index: 1000;
-        }
-
-        h1 {
-            font-size: 2em;
-            margin-bottom: 20px;
-        }
-
-        p {
-            font-size: 1.2em;
-            margin-bottom: 10px;
-        }
-
-        a {
-            color: #0066cc;
-            text-decoration: none;
-            font-size: 1.2em;
-        }
-
-        footer {
-            margin-top: 10px;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/404Style.css">
 </head>
 
 <body>
