9d6d5b06c1f8f5c937caedbae835965072f5134e
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-02-09 20:23:29 -0300

    se trabaja con composer

---
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/6-ejemploComposer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eduardo/6-ejemplo-composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd8863dc87bdba2c0cb99dcfcd2eef895d53f63b1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eduardo/6-ejemplo-composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd8863dc87bdba2c0cb99dcfcd2eef895d53f63b1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
