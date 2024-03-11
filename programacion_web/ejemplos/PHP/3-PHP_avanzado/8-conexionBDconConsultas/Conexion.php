27ca62d9a01d15242c5889ac54a0d720d5e474d0
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-03-11 07:26:55 -0300

    se sube la prueba diagnostica

---
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/8-conexionBDconConsultas/Conexion.php
@@ -0,0 +1,62 @@
+<?php
+
+// configuración de la base de Datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'finanzas');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+class Conexion {
+
+    private static $instance;
+    private $connection;
+
+    private function __construct() {
+        $this->makeConnection();
+    }
+
+    public static function getInstance() {
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getDatabaseInstance() {
+        return $this->connection;
+    }
+
+    public function closeConnection() {
+        $this->connection = null;
+    }
+
+    private function makeConnection() {
+        $server = DB_HOST;
+        $database = DB_NAME;
+        $username = DB_USER;
+        $password = DB_PASS;
+
+        try {
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ];
+
+            $conexion = new PDO("mysql:host=$server;dbname=$database", $username, $password, $options);
+
+            $setnames = $conexion->prepare("SET NAMES 'utf8'");
+            $setnames->execute();
+
+            $this->connection = $conexion;
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function __destruct() {
+        $this->closeConnection();
+    }
+}
+?>
---
+++ programacion_web/ejemplos/PHP/3-PHP_avanzado/8-conexionBDconConsultas/LogicaConsultas.php
@@ -0,0 +1,92 @@
+<?php
+
+class LogicaConsultas
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = Conexion::getInstance()->getDatabaseInstance();
+    }
+
+    public function traerRegistros()
+    {
+        try {
+            $consulta = $this->conn->prepare("SELECT * FROM ingresos;");
+            $consulta->execute();
+            $resultados = $consulta->fetchAll();
+        } catch (PDOException $e) {
+            echo "Error al obtener la lista de ingresos: " . $e->getMessage();
+        }
+    }
+
+
+    public function guardarRegistros($data)
+    {
+        try {
+            $consulta = $this->conn->prepare("INSERT INTO ingresos(metodo_pago, tipo, fecha_ingreso, cantidad, descripcion) VALUES (:metodo_pago, :tipo, :fecha_ingreso, :cantidad, :descripcion);");
+            $consulta->bindValue(":metodo_pago", $data['metodo_pago']);
+            $consulta->bindValue(":tipo", $data['tipo']);
+            $consulta->bindValue(":fecha_ingreso", $data['fecha_ingreso']);
+            $consulta->bindValue(":cantidad", $data['cantidad']);
+            $consulta->bindValue(":descripcion", $data['descripcion']);
+            $consulta->execute();
+            header("location: ingresos");
+        } catch (PDOException $e) {
+            echo "Error al almacenar el ingreso: " . $e->getMessage();
+        }
+    }
+
+    public function mostrarUnRegistro($id)
+    {
+        try {
+            $consulta = $this->conn->prepare("SELECT * FROM ingresos WHERE id=:id;");
+            $consulta->execute([":id" => $id]);
+            $resultados = $consulta->fetch();
+            if (!$resultados) {
+                //Lanzamos una excepción zuculenta si no existe el valor
+                throw new Exception("No se encontró el dato con ID: $id");
+            }
+        } catch (Exception $e) {
+            // Se captura la excepción
+            echo "Se ha producido una excepción: " . $e->getMessage();
+        }
+    }
+
+    public function actualizarRegistro($data)
+    {
+        try {
+            $consulta = $this->conn->prepare("UPDATE ingresos SET 
+            metodo_pago = :metodo_pago,
+            tipo = :tipo,
+            fecha_ingreso = :fecha_ingreso,
+            cantidad = :cantidad,
+            descripcion = :descripcion WHERE id=:id;");
+            $consulta->execute([
+                ":id" => $data['id'],
+                ":metodo_pago" => $data['metodo_pago'],
+                ":tipo" => $data['tipo'],
+                ":fecha_ingreso" => $data['fecha_ingreso'],
+                ":cantidad" => $data['cantidad'],
+                ":descripcion" => $data['descripcion'],
+            ]);
+        } catch (PDOException $e) {
+            echo "Error al actualizar el ingreso: " . $e->getMessage();
+        }
+    }
+
+    public function eliminarRegistro($data)
+    {
+        $id = $data['id'];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            try {
+                $consulta = $this->conn->prepare("DELETE FROM ingresos WHERE id=:id;");
+                $consulta->execute([":id" => $id]);
+            } catch (PDOException $e) {
+                echo "Error al eliminar el ingreso: " . $e->getMessage();
+            }
+        } else {
+            echo "Método no permitido";
+        }
+    }
+}
