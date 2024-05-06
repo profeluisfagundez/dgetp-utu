2a22dd90d661b6546bbf81faea05448e689cdfdb
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-05-06 15:40:40 -0300

    se agrega el practico 3

---
+++ CTT-REDES-Y-SOFTWARE/clases/3-estructura-for/index.php
@@ -0,0 +1,18 @@
+<?php
+//Estructura FOR
+
+$variableDos = 0;
+for ($i = 20; $i > $variable; $i--) {
+    echo $i . " "; 
+}
+
+echo "<br>";
+
+$variable = 15;
+for ($i = 0; $i <= $variable; $i++) {
+    echo $i . " "; 
+}
+
+
+
+
