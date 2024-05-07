589718f605df54d37b94a4df6e0c31b94e94fc45
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-05-07 10:06:25 -0300

    se empieza a trabajar en un ejemplo

---
+++ CTT-REDES-Y-SOFTWARE/clases/otros/miprimerweb/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$arreglo = [10,'Luis',true,3.141516,"Eduardo"];
+echo "<pre>";
+var_dump($arreglo);
+echo "</pre>";
+?>
--- CTT-REDES-Y-SOFTWARE/practicos/practico3/index.php
+++ CTT-REDES-Y-SOFTWARE/practicos/practico3/index.php
@@ -13,7 +13,8 @@ for ($i = 10; $i >= 1; $i--) {
 //3 - Calcular la suma de los primeros 10 números naturales:
 $suma = 0;
 for ($i = 1; $i <= 10; $i++) {
-    $suma += $i;
+    //$suma += $i;
+    $suma = $suma + $i;
 }
 echo "La suma de los primeros 10 números naturales es: " . $suma;
 
