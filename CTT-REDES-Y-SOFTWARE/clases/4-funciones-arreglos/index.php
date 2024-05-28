fc946b905d26d352556eba7a11dfa795852c111e
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-05-27 21:57:44 -0300

    se agrega un ejemplo de explode

--- CTT-REDES-Y-SOFTWARE/clases/4-funciones-arreglos/index.php
+++ CTT-REDES-Y-SOFTWARE/clases/4-funciones-arreglos/index.php
@@ -65,4 +65,10 @@ sort($arregloFrutas);
 // rsort: Ordena un array en orden descendente.
 rsort($arregloFrutas);
 
+//Explode: permite crear un arreglo en base a un separador en una variable
+$pizza  = "porción1 porción2 porción3 porción4 porción5 porción6";
+$porciones = explode(" ", $pizza);
+echo $porciones[0]; // porción1
+echo $porciones[1]; // porción2
+
 ?>
