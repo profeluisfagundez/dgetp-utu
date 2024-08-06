4ea5dc1723382511c12bd36f7e9d08c800e218a8
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-08-06 19:20:31 -0300

    se encuentra el error en el login

--- CTT-REDES-Y-SOFTWARE/FRAY-BENTOS/proyectoEgreso/app/views/usuarios/index.php
+++ CTT-REDES-Y-SOFTWARE/FRAY-BENTOS/proyectoEgreso/app/views/usuarios/index.php
@@ -1,18 +1,19 @@
 <?php
-/*require_once("../../controllers/LoginController.php");
-
+session_start();
+require_once("../../controllers/LoginController.php");
 $loginController = new LoginController();
-$loginController->checkUserLoggedIn();*/
+$loginController->checkUserLoggedIn();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página princial del usuario</title>
+    <title>Menú de administración de usuarios</title>
 </head>
 <body>
-    <h1>Bienvenido Usuario</h1>
+    <h1>Bienvenido, <?php echo $_SESSION['usuario']['username']; ?></h1>
+    <p>Esta es una página protegida solo accesible para usuarios registrados.</p>
 </body>
 </html>
