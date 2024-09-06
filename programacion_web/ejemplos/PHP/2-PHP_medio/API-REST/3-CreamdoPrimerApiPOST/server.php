6d8b7295cb2ba6aabb5cff7fdb329ff8498001d3
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-09-06 16:04:15 -0300

    se agrega la segunda parte de api

---
+++ programacion_web/ejemplos/PHP/2-PHP_medio/API-REST/3-CreamdoPrimerApiPOST/server.php
@@ -0,0 +1,60 @@
+<?php
+
+// Definimos los recursos disponibles para hacer la petición
+$allowedResourceType = [
+    'books',
+    'authors',
+    'genres',
+];
+
+// Validamos que el recurso este disponible en nuestro servidor
+$resourceType = $_GET['resource_type'];
+
+//Preguntamos, si existe el recurso en el array, seguimos, sino, detenemos la app
+if (!in_array($resourceType, $allowedResourceType)) {
+    die;
+}
+
+// Defino los recursos que mencionamos arriba
+$books = [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    2 => [
+        'titulo' => 'La Iliada',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+];
+
+// Se indica al cliente que lo que recibirá es un json
+header('Content-Type: application/json');
+
+// Levantamos el id del recurso buscado
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
+
+// Generamos la respuesta asumiendo que el pedido es correcto, mucho cuidado con los nombres de las variables
+switch (strtoupper($_SERVER['REQUEST_METHOD'])) {
+    case 'GET':
+        if (empty($resourceId)) {
+            echo json_encode($books);
+        } else {
+            if (array_key_exists($resourceId, $books)) {
+                echo json_encode($books[$resourceId]);
+            }
+        }
+        break;
+    case 'POST':
+        break;
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+}
