e1dcda1de4da26af571e81de8e646bbf5535d94b
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2025-03-19 09:11:03 -0300

    se agrega informacion

--- tecnologoSeguridadInformatica/ejemplo.sh
+++ tecnologoSeguridadInformatica/ejemplo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# shebang 
-read -p "Ingrese el usuario del sistema" usuario
+read -p "Ingrese el usuario del sistema" usuarioIngresado
+usuario=/home/$usuarioIngresado
 if [ -d $usuario ]; then
     mkdir -p /home/$USER/Documentos/seguridadInformatica
     echo "Se ha creado el directorio"
 else
     echo "Ingrese un usuario valido"
 fi
+w
+# chmod +x ejemplo.sh para dar permisos
+# ./ejemplo.sh para ejecutar
+# setxkbmap es
---
+++ tecnologoSeguridadInformatica/ejercicio12.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Crear un script que tenga una variable llamada pin, la cual su valor será 1234, el usuario deberá de ingresar un número y este se deberá de comparar con el pin, si el pin es correcto deberá de listar todo el contenido de su home en forma de árbol, en caso de no coincidir con el pin deberá de decirle al usuario que el pin ingresado es incorrecto.
+#
+
