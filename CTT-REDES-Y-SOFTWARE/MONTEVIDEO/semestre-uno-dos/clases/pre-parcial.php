b8a629ee4bfe54f4111e748713bb0a081c76d85b
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2025-05-15 08:53:28 -0300

    se trabaja con el pre parcial

--- CTT-REDES-Y-SOFTWARE/MONTEVIDEO/semestre-uno-dos/clases/pre-parcial.php
+++ CTT-REDES-Y-SOFTWARE/MONTEVIDEO/semestre-uno-dos/clases/pre-parcial.php
@@ -6,46 +6,46 @@ $variableBooleano = true; // Booleano
 $variableString = "Hola Mundo"; // String
 $variableFlotante = 10.5; // Flotante
 
-//echo "Variable Entera: $variableEntera <br>";
-//echo "Variable Booleano: $variableBooleano <br>";
-//echo "Variable String: $variableString <br>";
-//echo "Variable Flotante: $variableFlotante <br>";
+echo "Variable Entera: $variableEntera <br>";
+echo "Variable Booleano: $variableBooleano <br>";
+echo "Variable String: $variableString <br>";
+echo "Variable Flotante: $variableFlotante <br>";
 
-//var_dump($variableEntera);
-//echo "<br>";
-//var_dump($variableBooleano);
-//echo "<br>";
-//var_dump($variableString);
-//echo "<br>";
-//var_dump($variableFlotante);
-//echo "<br>";
+var_dump($variableEntera);
+echo "<br>";
+var_dump($variableBooleano);
+echo "<br>";
+var_dump($variableString);
+echo "<br>";
+var_dump($variableFlotante);
+echo "<br>";
 
 //****************************************************************
-//if ($variableEntera <= 8) {
-//    echo "El valor de la variable es menor o igual a 10 <br>";
-//}
+if ($variableEntera <= 8) {
+    echo "El valor de la variable es menor o igual a 10 <br>";
+}
 
-//if ($variableBooleano == true) {
-//    echo "El valor de la variable es: true<br>";
-//} else {
-//    echo "El valor de la variable es: false<br>";
-//}
+if ($variableBooleano == true) {
+    echo "El valor de la variable es: true<br>";
+} else {
+    echo "El valor de la variable es: false<br>";
+}
 
 //Dada dos variables enteras, mostrar el mayor de los dos
-//$numero1 = 10;
-//$numero2 = 20;
-//if ($numero1 > $numero2) {
-//    echo "El mayor es: $numero1 <br>";
-//} else {
-//    echo "El mayor es: $numero2 <br>";
-//}
+$numero1 = 10;
+$numero2 = 20;
+if ($numero1 > $numero2) {
+    echo "El mayor es: $numero1 <br>";
+} else {
+    echo "El mayor es: $numero2 <br>";
+}
 
 //Estructura de control for
-//for ($i = 0; $i <= 10; $i++) {
-//    echo "El valor de i es: $i <br>";
-//}
+for ($i = 0; $i <= 10; $i++) {
+    echo "El valor de i es: $i <br>";
+}
 
-// < menor que
+//< menor que
 // > mayor que
 // <= menor o igual que
 // >= mayor o igual que
@@ -54,15 +54,15 @@ $variableFlotante = 10.5; // Flotante
 // != diferente que
 
 $arregloDePalabras = ['Pedro', 'Jose', 'Maikol', 'Maria', 'Pepito'];
-//var_dump($arregloDePalabras);
-//echo "<br>";
-//echo "Posición 2: ";
-//echo $arregloDePalabras[2];
-//echo "<br>";
+var_dump($arregloDePalabras);
+echo "<br>";
+echo "Posición 2: ";
+echo $arregloDePalabras[2];
+echo "<br>";
 
-//foreach ($arregloDePalabras as $indice){
-//    echo "Palabra: $indice <br>";
-//}
+foreach ($arregloDePalabras as $indice){
+    echo "Palabra: $indice <br>";
+}
 
 echo count($arregloDePalabras);
 echo "<br>";
@@ -70,4 +70,3 @@ echo "<br>";
 for($i = 0; $i < count($arregloDePalabras); $i++){
     echo "Palabra: $arregloDePalabras[$i] <br>";
 }
-
